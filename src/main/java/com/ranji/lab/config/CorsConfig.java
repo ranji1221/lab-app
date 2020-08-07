08432f3eff686d6cdfafaaa20e1c192e10fc76ee
jiran1221 <jiran1221@163.com>
2020-08-07 10:12:38 +0800

    解决跨域访问的问题

---
+++ src/main/java/com/ranji/lab/config/CorsConfig.java
@@ -0,0 +1,65 @@
+package com.ranji.lab.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ *  全局跨域配置
+ *  注意：全局跨域配置后会触发两个问题
+ *  1、它会打破SpringBoot的web自动配置，导致静态资源访问不到，所以在这里要自己进行静态资源路径映射
+ *  2、它会导致Swagger接口文档无法访问，被拦截，所以要自行处理下接口文档不能访问的问题
+ *  3、如果不是全部的接口需要跨域，那么还是建议采用第一种跨域的解决方案，利用@CrossOrigin注解标注即可
+ *     就无需配置这个配置类，该注解可以标注类或者防范，做到精细化的控制接口是否需要跨域
+ *  @author RanJi
+ *  @since jdk8
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurationSupport {
+    /**
+     * 全局跨域处理
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+
+    /**
+     * 一旦配置了跨域问题，就会涉及到静态资源的访问处理
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //-- 第一种映射静态资源的方法
+        /*
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/public/")
+        ;
+        */
+        //-- 第二种映射静态资源的方法
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("classpath:/static/image/");
+
+        //-- 处理Swagger接口文档无法访问的问题
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        super.addResourceHandlers(registry);
+    }
+
+}
--- src/main/java/com/ranji/lab/controller/LoginController.java
+++ src/main/java/com/ranji/lab/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    /**
+     * 第一种解决跨域问题的解决方案，直接在某个要访问的接口路径上加@CrossOrigin注解
+     * @return
+     */
+    //@CrossOrigin
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String toLogin(){
         return "login";
