c9b1b8f8caec1e1ee7cdf2e5c0a560ffed53d00a
DESKTOP-4UKN0O8\Administrator <330028186@qq.com>
2020-08-24 20:57:35 +0800

    [更新部分接口]

--- src/main/java/com/ranji/lab/mapper/LaboratoryDeviceMapper.java
+++ src/main/java/com/ranji/lab/mapper/LaboratoryDeviceMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Update;
 import java.util.List;
 
 public interface LaboratoryDeviceMapper {
-    @Insert("insert into laboratory_device (laboratory_id,device_id,status) values (#{laboratoryId},#{deviceId,#{status}})")
+    @Insert("insert into laboratory_device (laboratory_id,device_id,status) values (#{laboratoryId},#{deviceId},#{status})")
     int insertLaboratoryDevice(LaboratoryDevice laboratoryDevice);
     @Update("update laboratory_device set laboratory_id = #{laboratoryId},device_id=#{deviceId},status=#{status} where id = #{id}")
     int updateLaboratoryDevice(LaboratoryDevice laboratoryDevice);
