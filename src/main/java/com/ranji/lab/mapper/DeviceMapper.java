9fb82fc2ca7428c48fc820e41e89dff42b658671
DESKTOP-4UKN0O8\Administrator <330028186@qq.com>
2020-09-14 15:53:18 +0800

    [修改] 修改接口 [新增] 新增權限

--- src/main/java/com/ranji/lab/mapper/DeviceMapper.java
+++ src/main/java/com/ranji/lab/mapper/DeviceMapper.java
@@ -112,7 +112,7 @@ public interface DeviceMapper {
     LaboratoryDeviceNumDto findDeviceStatusNum(int laboratoryId,int deviceModelId,int status);
 
     //按照实验室id查询该实验室已完成的实验数量
-    @Select("select count(*) from project_device pd join arrange a on pd.arrange_project_id = a.id where a.status = 2 and a.laboratory_id = #{laboratoryId}")
+    @Select("select count(*) from arrange a where where a.status = 2 and a.laboratory_id = #{laboratoryId}")
     int findEndingProjectNumByLaboratoryId(int laboratoryId);
 
     //按照实验室id、设备id查询该实验室使用到该设备的实验数量
